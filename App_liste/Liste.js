2803fbb3e05bb3b4f5940ce0f70cce10fef9ddf2
Xepick <mgasquet@live.fr>
2019-02-26 14:52:50 +0100

    Commit liste

--- App_liste/Liste.js
+++ App_liste/Liste.js
@@ -1,12 +1,44 @@
+var app= angular.module("ListeApp", []);
 
-ListeApp.controller('TodoCtrl', ['$scope', function($scope){
+app.controller('TodoCtrl', ['$scope', function($scope){
 
-    $scope.init()= function(){
-        $scope.taskSet=[];
-        $scope.moncompteur=0;
-    };
+    // On crée un tableau t'objets appelé taskSet
+    $scope.taskSet=[{
+        taskName:"Faire le menage",
+        done:false
+    }];
+    
+    $scope.addTask=function()
+    {
+        
+    if($scope.taskName!="")
+    {
+        $scope.taskSet.push({
+            taskName:$scope.taskName,
+            done:false
+        });
 
-    $scope.addTask()=function(){
+        // Vide le champ
+        $scope.taskName="";
+    }
+    }
+    $scope.finishTask=function(ta){
+        console.log(ta);
+        ta.done = !ta.done;
+        if(ta.done==true)
+            $scope.styleTa = {
+                "text-decoration":"line-through"
+            }
+        else{
+            $scope.styleTa = {
+                "text-decoration":"none"
+            }
+        }
+        //$scope.index=$scope.taskSet.findIndex(taskSet => taskSet.taskName === $scope.ta);
 
-    };
+       
+
+
+        
+    }
 }]);
